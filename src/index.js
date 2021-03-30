86dc1157d0e26292b69540451fa1f9cd917006af
dapeshcode <ampeccia@gmail.com>
2021-03-30 01:32:01 -0500

    audio

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ const mainDiv = document.querySelector('div#main')
 const logo = document.querySelector('img#logo')
 const startButton = document.querySelector('button#start')
 const charDiv = document.querySelector('div#characters')
+const createCharacterForm = document.querySelector('form#create-character')
+const items = document.querySelector('div#items')
 
 
 // ************************ pages***********************
@@ -18,7 +20,9 @@ const page2 = document.querySelector('div[data-id="2"]')
 const page3 = document.querySelector('div[data-id="3"]')
 const page4 = document.querySelector('div[data-id="4"]')
 const page5 = document.querySelector('div[data-id="5"]')
-const page6 = document.querySelector('div#end')
+const page6 = document.querySelector('div[data-id="6"]')
+const page7 = document.querySelector('div#end')
+
 
 
 // ************************ init ***********************
@@ -54,18 +58,23 @@ function login () {
 }
 
 
-// ************************ event listeners ***********************
+// ******************* event listeners ********************
+// ******************* el main page ************************
 
 mainDiv.addEventListener('click', (e) => {
     if (e.target.matches('button#start')){
-        // debugger
+    
         const id = parseInt(e.target.dataset.id)
         startGame(id);
         mainDiv.style.display = "none"
+        items.style.display = "block"
+
+
+
         
         
         
-        console.log("start")
+        
     } else if (e.target.matches('button#help')){
 
         help();
@@ -93,7 +102,94 @@ charDiv.addEventListener('click', (e) => {
 
 
 
-// ************************ game page ***********************
+
+createCharacterForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+   const name = e.target.name.value
+   const image = e.target.image.value
+   const user_id = logo.dataset.id
+    
+   const newChar = {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    },
+    body: JSON.stringify({name, image, user_id})
+}
+    fetch(charsUrl, newChar)
+        .then(res => res.json())
+        .then(charObj => {
+            renderOneCharacter(charObj)
+            console.log(charObj)
+        })
+
+})
+// ******************* el page 4 ********************
+page4.addEventListener('click', (e) => {
+    if (e.target.matches('button#galaxy')) {
+        const audio = e.target.previousElementSibling
+        audio.play()
+        //update lose life
+        //if lives == 0, game over screen
+
+    } else if(e.target.matches('button#saturn')) {
+        const audio = e.target.previousElementSibling
+        audio.play()
+        pageFive()
+
+
+    } else if(e.target.matches('button#rolled')) {
+        const audio = e.target.previousElementSibling
+        audio.play()
+        //update lose life
+        //if lives == 0, game over screen
+        page4.style.display = "none"
+        pageOne()
+        
+    }
+})
+// ******************* el temp ********************
+document.addEventListener('click', (e) => {
+
+    //43 update starbux fetch and update the view
+    const id = parseInt(startButton.dataset.id)
+    if (e.target.matches('button[id="1"]')) {
+        fetch(`${charsUrl}/${id}`, { 
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify({starbux: 3})
+        })
+            .then(res => res.json())
+            .then(charObj => console.log(charObj))
+
+        
+        pageTwo()
+
+    } else if (e.target.matches('button[id="2"]')) {
+        pageThree()
+
+    } else if (e.target.matches('button[id="3"]')) {
+        pageFour()
+    }  else if (e.target.matches('button[id="5"]')) {
+        end()
+    } else if (e.target.matches('button[id="6"]')) {
+        pageSeven()
+    } else if (e.target.matches('button[id="7"]')) {
+        //43 add back to main function
+        mainPage()
+    }
+
+
+
+})
+
+
+
+// ************************ game page **********************
 
 function mainPage (user) {
 
@@ -109,18 +205,14 @@ function mainPage (user) {
     .then(userChars => renderAllCharacters(userChars))
 }
 
+// ************************ helper ***********************
 // ************************ start ***********************
 
-
-
 function startGame (id) {
-    // debugger
 
     fetch(`${charsUrl}/${id}`)
         .then(res => res.json())
         .then(({current_state}) => currentState(current_state))
-
-
 }
 
 
@@ -149,6 +241,8 @@ function currentState (current_state) {
     }
 
 }
+
+// ************************ pages ***********************
 
 function pageOne () {
     
@@ -185,46 +279,27 @@ function pageFive () {
     updateCurrentState(5)
 }
 
-function end () {
+function pageSix () {
     
     page6.style.display = "block"
     page5.style.display = "none"
+    updateCurrentState(6)
+}
+
+function end () {
+    
+    page7.style.display = "block"
+    page6.style.display = "none"
     updateCurrentState(0)
 }
 
-document.addEventListener('click', (e) => {
-
-    if (e.target.matches('button[id="1"]')) {
-        pageTwo()
-    } else if (e.target.matches('button[id="2"]')) {
-        pageThree()
-
-    } else if (e.target.matches('button[id="3"]')) {
-        pageFour()
-    } else if (e.target.matches('button[id="4"]')) {
-        pageFive()
-    } else if (e.target.matches('button[id="5"]')) {
-        end()
-    }
 
 
-})
 
-function updateCurrentState (currentPage) {
-    const id = parseInt(logo.dataset.id)
-    fetch(`${usersUrl}/${id}`, {
-        method: 'PATCH',
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-        }, 
-        body: JSON.stringify({current_state: currentPage})
-    })
-    .then(res => res.json())
-    .then(res => console.log(res))
-}
 
 /*** HELPER METHODS ***/
+// ************************ render ***********************
+
 
     function renderOneCharacter(charObj){
         const show = document.querySelector('ul#show')
@@ -241,4 +316,20 @@ function updateCurrentState (currentPage) {
         userChars.forEach(charObj => {
             renderOneCharacter(charObj)
         })
+    }
+
+
+    //43 fix this patch
+    function updateCurrentState (currentPage) {
+        const id = parseInt(startButton.dataset.id)
+        fetch(`${charsUrl}/${id}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }, 
+            body: JSON.stringify({current_state: currentPage})
+        })
+        .then(res => res.json())
+        .then(res => console.log(res))
     }
